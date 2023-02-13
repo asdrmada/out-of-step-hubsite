70ed808d8b9e8b83d6107d5c5a74b56a289072fb
asdrmada <42623227+asdrmada@users.noreply.github.com>
2023-02-13 19:56:10 +0000

    intellisense now functioning, proceeding with layout

--- pages/index.js
+++ pages/index.js
@@ -40,8 +40,20 @@ export default function Home() {
       </div>
 
       <main className={styles.main}>
-        <card className='w-30 h-30 m-5'>
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+        <card className='w-2/3 h-30 m-5 p-3 border border-black right-auto'>
+        <div>
+          <h2 className='text-3xl font-semibold'>Post title, innit!</h2>
+          <br className='grey'></br>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
+        </card>
+
+        <card className='w-2/3 h-30 m-5 p-3 border border-black right-auto'>
+        <div>
+          <h2 className='text-3xl font-semibold'>Post title, innit!</h2>
+          <br className='grey'></br>
+          <p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+        </div>
         </card>
 
       </main>
